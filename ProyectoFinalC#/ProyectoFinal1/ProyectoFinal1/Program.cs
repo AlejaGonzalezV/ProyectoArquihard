b93801e786b6d99a8922217bd0b119d4144ed96a
Alejandra Gonzalez <aleja.gonzalezv19@gmail.com>
2020-12-06 14:34:25 -0500

    Cambios

--- ProyectoFinalC#/ProyectoFinal1/ProyectoFinal1/Program.cs
+++ ProyectoFinalC#/ProyectoFinal1/ProyectoFinal1/Program.cs
@@ -17,13 +17,13 @@ namespace ProyectoFinal1
         VERSIÓN 1
         */
  
-        static void AlgoritmoVersion_1(String src, int tamanio)
+        static void AlgoritmoVersion_1(String src)
         {
 
             /*
             Variable donde se almacenan los bits de la imagen.
             */
-            Bitmap imagenOriginal = new Bitmap("./im1.bmp");
+            Bitmap imagenOriginal = new Bitmap(src);
 
             Bitmap a = imagenOriginal;
 
@@ -34,9 +34,9 @@ namespace ProyectoFinal1
             stopWatch.Restart();
             stopWatch.Start();
 
-            for (int i = 0; i < a.Width - 1; i++)
+            for (int i = 0; i < a.Width; i++)
             {
-                for (int j = 0; j < a.Height - 1; j++)
+                for (int j = 0; j < a.Height; j++)
                 {
                     color = a.GetPixel(i, j);
                     color = Color.FromArgb((255 - color.R), (255 - color.G), (255 - color.B));
@@ -50,14 +50,14 @@ namespace ProyectoFinal1
             Console.WriteLine("Algoritmo Version 1: " + tiempo);
 
 
-            a.Save("./imagenesInvertidas/" + tamanio + ".bmp");
+            a.Save(@"C:\Users\aleja\Desktop\imalg1inv.bmp");
         }
 
         /*
         VERSIÓN 2
         */
 
-        static void AlgoritmoVersion_2(String src, int tamanio)
+        static void AlgoritmoVersion_2(String src)
         {
             /*
              Variable donde se almacenan los bits de la imagen.
@@ -73,34 +73,34 @@ namespace ProyectoFinal1
             stopWatch.Restart();
             stopWatch.Start();
 
-            for (int i = 0; i < a.Height - 1; i++)
+            for (int i = 0; i < a.Width; i++)
             {
-                for (int j = 0; j < a.Width - 1; j++)
+                for (int j = 0; j < a.Height; j++)
                 {
                     color = a.GetPixel(i, j);
-                    color = Color.FromArgb(255, (255 - color.R), color.G, color.B);
+                    color = Color.FromArgb((255 - color.R), color.G, color.B);
                     a.SetPixel(i, j, color);
                 }
             }
 
 
-            for (int i = 0; i < a.Height - 1; i++)
+            for (int i = 0; i < a.Width; i++)
             {
-                for (int j = 0; j < a.Width - 1; j++)
+                for (int j = 0; j < a.Height; j++)
                 {
                     color = a.GetPixel(i, j);
-                    color = Color.FromArgb(255, color.R, (255 - color.G), color.B);
+                    color = Color.FromArgb(color.R, (255 - color.G), color.B);
                     a.SetPixel(i, j, color);
                 }
             }
 
 
-            for (int i = 0; i < a.Height - 1; i++)
+            for (int i = 0; i < a.Width; i++)
             {
-                for (int j = 0; j < a.Width - 1; j++)
+                for (int j = 0; j < a.Height; j++)
                 {
                     color = a.GetPixel(i, j);
-                    color = Color.FromArgb(255, color.R, -color.G, (255 - color.B));
+                    color = Color.FromArgb(color.R, color.G, (255 - color.B));
                     a.SetPixel(i, j, color);
                 }
             }
@@ -111,13 +111,13 @@ namespace ProyectoFinal1
             tiempo = (long)(stopWatch.Elapsed.TotalMilliseconds * 1000000);
             Console.WriteLine("Algoritmo Version 2: " + tiempo);
 
-            a.Save("./imagenesInvertidas/" + tamanio + ".bmp");
+            a.Save(@"C:\Users\aleja\Desktop\imgalg2inv.bmp");
         }
 
         /*
         VERSIÓN 3
         */
-        static void AlgoritmoVersion_3(String src, int tamanio)
+        static void AlgoritmoVersion_3(String src)
         {
             /*
              Variable donde se almacenan los bits de la imagen.
@@ -134,9 +134,9 @@ namespace ProyectoFinal1
             stopWatch.Start();
 
 
-            for (int j = 0; j < a.Height - 1; j++)
+            for (int j = 0; j < a.Height; j++)
             {
-                for (int i = 0; i < a.Width - 1; i++)
+                for (int i = 0; i < a.Width; i++)
                 {
                     color = a.GetPixel(i, j);
                     color = Color.FromArgb((255 - color.R), (255 - color.G), (255 - color.B));
@@ -148,16 +148,16 @@ namespace ProyectoFinal1
 
 
             tiempo = (long)(stopWatch.Elapsed.TotalMilliseconds * 1000000);
-            Console.WriteLine("Algoritmo Version 2: " + tiempo);
+            Console.WriteLine("Algoritmo Version 3: " + tiempo);
 
-            a.Save("./imagenesInvertidas/" + tamanio + ".bmp");
+            a.Save(@"C:\Users\aleja\Desktop\imgalg3inv.bmp");
 
         }
 
         /*
         VERSIÓN 4
         */
-        static void AlgoritmoVersion_4(String src, int tamanio)
+        static void AlgoritmoVersion_4(String src)
         {
 
             /*
@@ -175,23 +175,23 @@ namespace ProyectoFinal1
             stopWatch.Start();
 
 
-            for (int i = 0; i < a.Height - 1; i++)
+            for (int i = 0; i < a.Width ; i++)
             {
-                for (int j = 0; j < a.Width - 1; j++)
+                for (int j = 0; j <  a.Height; j++)
                 {
                     color = a.GetPixel(i, j);
-                    color = Color.FromArgb(255, (255 - color.R), color.G, color.B);
+                    color = Color.FromArgb((255 - color.R), color.G, color.B);
                     a.SetPixel(i, j, color);
                 }
             }
 
 
-            for (int i = 0; i < a.Height - 1; i++)
+            for (int i = a.Width-1; i >= 0; i--)
             {
-                for (int j = 0; j < a.Width - 1; j++)
+                for (int j = a.Height-1; j >= 0; j--)
                 {
                     color = a.GetPixel(i, j);
-                    color = Color.FromArgb(255, color.R, (255 - color.G), (255 - color.B));
+                    color = Color.FromArgb(color.R, (255 - color.G), (255 - color.B));
                     a.SetPixel(i, j, color);
                 }
             }
@@ -199,17 +199,17 @@ namespace ProyectoFinal1
             stopWatch.Stop();
 
             tiempo = (long)(stopWatch.Elapsed.TotalMilliseconds * 1000000);
-            Console.WriteLine("Algoritmo Version 2: " + tiempo);
+            Console.WriteLine("Algoritmo Version 4: " + tiempo);
 
 
-            a.Save("./imagenesInvertidas/" + tamanio + ".bmp");
+            a.Save(@"C:\Users\aleja\Desktop\imgalg4inv.bmp");
 
         }
 
         /*
         VERSIÓN 5
         */
-        static void AlgoritmoVersion_5(String src, int tamanio)
+        static void AlgoritmoVersion_5(String src)
         {
             /*
              Variable donde se almacenan los bits de la imagen.
@@ -226,24 +226,24 @@ namespace ProyectoFinal1
             stopWatch.Start();
 
 
-            for (int i = 0; i < a.Height - 2; i += 2)
+            for (int i = 0; i < a.Width - 1; i += 2)
             {
-                for (int j = 0; j < a.Width - 2; j += 2)
+                for (int j = 0; j < a.Height - 1; j += 2)
                 {
                     color = a.GetPixel(i, j);
-                    color = Color.FromArgb(255, (255 - color.R), (255 - color.G), (255 - color.B));
+                    color = Color.FromArgb((255 - color.R), (255 - color.G), (255 - color.B));
                     a.SetPixel(i, j, color);
 
                     color = a.GetPixel(i, j + 1);
-                    color = Color.FromArgb(255, (255 - color.R), (255 - color.G), (255 - color.B));
+                    color = Color.FromArgb((255 - color.R), (255 - color.G), (255 - color.B));
                     a.SetPixel(i, j + 1, color);
 
                     color = a.GetPixel(i + 1, j);
-                    color = Color.FromArgb(255, (255 - color.R), (255 - color.G), (255 - color.B));
+                    color = Color.FromArgb((255 - color.R), (255 - color.G), (255 - color.B));
                     a.SetPixel(i + 1, j, color);
 
                     color = a.GetPixel(i + 1, j + 1);
-                    color = Color.FromArgb(255, (255 - color.R), (255 - color.G), (255 - color.B));
+                    color = Color.FromArgb((255 - color.R), (255 - color.G), (255 - color.B));
                     a.SetPixel(i + 1, j + 1, color);
 
                 }
@@ -253,9 +253,9 @@ namespace ProyectoFinal1
             stopWatch.Stop();
 
             tiempo = (long)(stopWatch.Elapsed.TotalMilliseconds * 1000000);
-            Console.WriteLine("Algoritmo Version 2: " + tiempo);
+            Console.WriteLine("Algoritmo Version 5: " + tiempo);
 
-            a.Save("./imagenesInvertidas/" + tamanio + ".bmp");
+            a.Save(@"C:\Users\aleja\Desktop\imgalg5inv.bmp");
 
         }
 
@@ -265,39 +265,35 @@ namespace ProyectoFinal1
 
             Console.WriteLine("Inserte versión del algoritmo: (ej: 1)");
             int version = int.Parse(Console.ReadLine());
-            
-            Console.WriteLine("Inserte el tamaño de la imagen: (ej: 640)");
-            int inputTamanio = int.Parse(Console.ReadLine());
 
-            //String imagenUtilizada = "./img/" + inputTamanio + ".bmp";
-            String imagenUtilizada = "./img/im1.bmp";
+            String imagenUtilizada = @"C:\Users\aleja\Desktop\mimir.bmp";
 
 
             switch (version)
                 {
                     case 1:
 
-                        AlgoritmoVersion_1(imagenUtilizada, inputTamanio);
+                        AlgoritmoVersion_1(imagenUtilizada);
                         break;
 
                     case 2:
 
-                        AlgoritmoVersion_2(imagenUtilizada, inputTamanio);
+                        AlgoritmoVersion_2(imagenUtilizada);
                         break;
 
                     case 3:
                        
-                        AlgoritmoVersion_3(imagenUtilizada, inputTamanio);
+                        AlgoritmoVersion_3(imagenUtilizada);
                         break;
 
                     case 4:
 
-                        AlgoritmoVersion_4(imagenUtilizada, inputTamanio);
+                        AlgoritmoVersion_4(imagenUtilizada);
                         break;
 
                     case 5:
 
-                        AlgoritmoVersion_5(imagenUtilizada, inputTamanio);
+                        AlgoritmoVersion_5(imagenUtilizada);
                         break;
 
                 }
